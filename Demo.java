72c833c0ce2f084a0efa03c0ee4f91d5a8edcbed
苟犇 <815166607@qq.com>
2019-12-17 20:46:35 +0800

    提交代码

---
+++ Demo.java
@@ -0,0 +1,11 @@
+public class Demo{
+    public static void main(String[] args){
+        int a = 1;
+        int b = 2;
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
---
+++ Test1.java
@@ -0,0 +1,8 @@
+public class Test1{
+    
+    public static void main(String[] args) {
+        int a = 1;
+        a += 10;
+        System.out.println(a);
+    }
+}
