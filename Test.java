8c145810e76f15064f07a7eb92835c289a0a01c4
苟犇 <815166607@qq.com>
2020-01-04 23:56:26 +0800

    提交代码

--- Test.java
+++ Test.java
@@ -1,11 +1,49 @@
-public class Test{
-  	public static void main(String[] args){
- 		int a =  1;
-		int b = 2;
-		a = a + b;
-		b = a - b;
-		a = a - b;
-	System.out.println(a);
-	System.out.println(b);
-}
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+       int[] arr = new int[]{9,3,2,5,4,6};
+       //System.out.println(binarySearch(arr, 5));
+        // System.out.println(avg(arr));
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static int binarySearch(int[] arr ,int toFind){
+        int left = 0;
+        int right = arr.length-1;
+        while (left <= right){
+            int mid = (left + right)/2;
+            if (toFind < arr[mid]){
+                right = mid -1;
+            }else if(toFind > arr[mid]){
+                left = mid +1;
+            }else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    public static double avg(int[] arr){
+        int sum = 0;
+        for (int i = 0;i < arr.length;i++){
+            sum += arr[i];
+        }
+        double avg =(double) sum/(arr.length);
+        return avg;
+    }
+
+    public static int[] bubbleSort(int[] arr){
+        for (int i = 0; i < arr.length;i++){
+            for (int j = arr.length-1;j>i;j--){
+                if (arr[j]<arr[j-1]){
+                    int tmp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = tmp;
+                }
+            }
+        }
+        return arr;
+    }
 }
