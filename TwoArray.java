673a5253d5e0e3eb7524b19c379885a8d5b0c3a2
苟犇 <815166607@qq.com>
2020-01-06 23:45:08 +0800

    提交代码

--- TwoArray.java
+++ TwoArray.java
@@ -1,7 +1,7 @@
 public class TwoArray {
     public static void main(String[] args) {
         int[][] arr = new int[][]{{1, 2, 3, 4}, {3, 2, 4}, {7, 8}};
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {   //普通for  循环遍历二维数组
             for (int j = 0; j < arr[i].length; j++) {
                 int value = arr[i][j];
                 System.out.print(value+"\t");
@@ -9,7 +9,7 @@ public class TwoArray {
             System.out.println();
         }
         System.out.println("--------我是一条华丽的分割符-----");
-        for (int[]array:arr){
+        for (int[]array:arr){//增强for  遍历二维数组
             for (int num:array){
                 System.out.print(num+"\t");
             }
