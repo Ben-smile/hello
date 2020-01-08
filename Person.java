6f39cbbc302f8d8d64e7da7e0e899b734fe45d26
苟犇 <815166607@qq.com>
2020-01-08 23:21:18 +0800

    提交代码

---
+++ Person.java
@@ -0,0 +1,45 @@
+package Ben.Test;
+
+public class Person {
+    public String name;
+    public int age;
+    public String sex;
+   //设计一个方法  画星星 倒直角三角
+    public void drawStar(int line,boolean direction){
+        for (int i = 1;i <= line;i++ ){
+            if (!direction) {
+                for (int j = 1; j <= i-1; j++) {
+                    System.out.print(" ");
+                }
+            }
+            for (int j = 0; j <= line - i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    //交换两个数组中的元素
+    public void changeArray (int[] a,int[] b){
+        int[] tmp = new int[a.length];
+        tmp = a;
+        a =  b;
+        b =  tmp;
+    }
+  //交换一个数组中头尾的位置
+    public void exchangeArray (int[] arr){
+        int left = 0;
+        int bound = arr.length-1;
+        while (left < bound){
+            int tmp = arr[left];
+            arr[left] = arr[arr.length-1];
+            arr[arr.length-1] = tmp;
+            left++;
+            bound--;
+        }
+        for (int value : arr){
+            System.out.print(value+"\t");
+        }
+    }
+
+
+}
---
+++ class-11.10。/Test.java
@@ -0,0 +1,11 @@
+public class Test{
+  	public static void main(String[] args){
+ 		int a =  1;
+		int b = 2;
+		a = a + b;
+		b = a - b;
+		a = a - b;
+	System.out.println(a);
+	System.out.println(b);
+}
+}
