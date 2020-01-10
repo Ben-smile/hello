8d1e18195507a4c37aadb5794d885bc66c7dff4c
苟犇 <815166607@qq.com>
2020-01-10 23:55:19 +0800

    提交代码

--- Functions.java
+++ Functions.java
@@ -1,6 +1,5 @@
 public class Functions {
-    
-//星星 倒直角三角
+    //星星 倒直角三角
     public void drawStar(int line,boolean direction){
         for (int i = 1;i <= line;i++ ){
             if (!direction) {
@@ -14,7 +13,7 @@ public class Functions {
             System.out.println();
         }
     }
-//交换两个数组中的元素
+        //交换两个数组中的元素
         public void changeArray (int[] a,int[] b){
             int[] tmp = new int[a.length];
             tmp = a;
@@ -115,6 +114,62 @@ public class Functions {
             return array;
         }
 
+        //设计一个方法 找寻一个区间的所有素数
+    public int[] primeNum (int begin,int end){
+        if (begin<=1||end <=1){    //判断区间  质数是指在大于1的自然数中，
+            return null;            // 除了1和它本身以外不再有其他因数的自然数。
+        }
+        if (begin >= end){          //区间的开始必须小于结束
+            return null;
+        }
+        int[] a = new int[begin+end];     //创建一个足够长的数组
+        int index = 0;             //记录 素数的个数
+        for (int i = begin; i<=end;i++){
+            boolean x = true;
+            for (int j = 2;j<=end;j++){
+                if (i%j==0 && i!=j){
+                    x = false;
+                    break;
+                }
+            }
+            if (x){               //若x标记没改变 则说明就是素数
+                a[index] = i;
+                index++;
+            }
+        }
+        int[] array = new int[index]; 
+        for (int i =0;i<array.length;i++){ // 去除足够长数组中的0元素
+            array[i] = a[i];
+        }
+        a = null;
+        return array;             //返回数组
+    }
+
+    //冒泡排序算法              待优化
+    public int[] bubbleSort (int[] a){
+        for (int i = 0;i<a.length-1;i++){
+            for (int j = a.length-1;j>i;j--){
+                if (a[j]<a[j-1]){
+                    int temp = a[j];
+                    a[j] = a[j-1];
+                    a[j-1] = temp;
+                }
+            }
+        }
+        return a;
+    }
+
+    //设计一个方法  做一个简单的登录验证
+    public String logon (String name,String password) {
+        String result = "用户名或密码错误";
+        String[][] arr = new String[][]{{"张三","123"},{"李四","666"},{"王五","888"}};
+        for (int i = 0;i<arr.length;i++){
+            if (arr[i][0].equals(name)&&arr[i][1].equals(password)){
+                result= "登录成功";
+            }
+        }
+        return result;
+    }
 }
 
 
