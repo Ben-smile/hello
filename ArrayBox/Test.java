d66d43c98159d1eb0ba55ac3426f76211b343b14
苟犇 <815166607@qq.com>
2020-01-12 23:50:26 +0800

    提交代码

--- ArrayBox/Test.java
+++ ArrayBox/Test.java
@@ -1,12 +1,12 @@
 public class Test {
     public static void main(String[] args){
-        ArrayBox box = new ArrayBox();
-        for (int i = 1;i <= 23;i++){
+        ArrayBox box = new ArrayBox();   //创建ArrayBox 对象
+        for (int i = 1;i <= 23;i++){     //通过 box.add()  存储元素
             box.add(i);
         }
         System.out.println("有效元素数为:"+box.size);
         System.out.println("Box的长度为:"+box.elementData.length);
-        for (int i = 0;i<box.size;i++){
+        for (int i = 0;i<box.size;i++){        //通过get方法S查看
             int value = box.get(i);
             System.out.println(value+" ");
         }
