72f2e5713c39e9c08877cee54d859455c84684fb
苟犇 <815166607@qq.com>
2020-01-15 22:14:27 +0800

    提交代码

--- Contain/Car.java
+++ Contain/Car.java
@@ -5,7 +5,6 @@ public class Car {
     public String color;
     public String type;
     public Wheel wheel;
-
     public void showCar(){
         System.out.println("这是一辆"+brand+"牌的"+color+"颜色的"+type+"型号的汽车");
         System.out.println("车上搭载着"+wheel.brand+"型号的"+wheel.color+"颜色的"+wheel.type+"型号的轮子");
