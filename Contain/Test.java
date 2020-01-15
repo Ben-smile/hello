990aba5bbf5376d9aaa379101509ab62708b2f8e
苟犇 <815166607@qq.com>
2020-01-15 22:15:11 +0800

    提交代码

--- Contain/Test.java
+++ Contain/Test.java
@@ -2,8 +2,7 @@ package CarAndWheel;
 
 public class Test {
     public static void main(String[] args){
-        Car car = new Car("宝马","红色","Z4",
-                new Wheel("米其林","45","黑色"));
+        Car car = new Car("宝马","红色","Z4",new Wheel("米其林","45","黑色"));
         car.showCar();
     }
 }
