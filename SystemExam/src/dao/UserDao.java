f46f25da08e79477554ba6286aadf794a3ad9bc2
苟犇 <815166607@qq.com>
2020-02-24 23:54:04 +0800

    提交代码

--- SystemExam/src/dao/UserDao.java
+++ SystemExam/src/dao/UserDao.java
@@ -6,8 +6,8 @@ import util.UserFileReader;
 public class UserDao {
 
 
-    //只用来读写数据
-    //负责查询一个人的信息
+     //只用来读写数据
+     //负责查询一个人的信息
     public User selectOne (String account){
 
         return UserFileReader.getUser(account);
