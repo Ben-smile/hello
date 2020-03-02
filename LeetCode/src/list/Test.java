93bcc52b669e27c6cdc9a94db76797be70225e37
苟犇 <815166607@qq.com>
2020-03-02 23:39:57 +0800

    提交代码

--- LeetCode/src/list/Test.java
+++ LeetCode/src/list/Test.java
@@ -87,6 +87,24 @@ public class Test {
         return cur;
     }
 
+public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dum = new ListNode(0), cur = dum;
+        while(l1 != null && l2 != null) {
+            if(l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            }
+            else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = l1 != null ? l1 : l2;
+        return dum.next;
+    }
+
+
 //将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if(l1 == null && l2 ==null){
