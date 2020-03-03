1c349ba1b1c442cf5791ab2dda346ae0758ab4fb
苟犇 <815166607@qq.com>
2020-03-03 23:43:00 +0800

    提交代码

--- LeetCode/src/list/Test.java
+++ LeetCode/src/list/Test.java
@@ -35,6 +35,16 @@ public class Test {
         return head;
     }
 
+//给你一个单链表的引用结点 head。链表中每个结点的值不是 0 就是 1。已知此链表是一个整数数//字的二进制表示形式。请你返回该链表所表示数字的 十进制值 。
+
+ public int getDecimalValue(ListNode head) {
+        int sum = 0;
+        while(head!=null){
+            sum = sum*2 + head.val;
+            head = head.next;
+        }
+        return sum;
+    }
 
 
     // 诸如链表为空等特殊情况一定要重点考虑到
