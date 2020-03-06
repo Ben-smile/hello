0a9d773de45fb57bec9b0413e54349d2b923af1b
苟犇 <815166607@qq.com>
2020-03-06 23:55:59 +0800

    提交代码

--- TestJDBC.java
+++ TestJDBC.java
@@ -1,9 +1,6 @@
 package test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.HashSet;
 
 public class TestJDBC {
@@ -23,9 +20,18 @@ public class TestJDBC {
             //4.创建 状态参数
             Statement stat =  conn.createStatement();
             //5.执行操作
-            stat.executeUpdate("update person set salary = 100 where username = '苟犇'");
-
+            //stat.executeUpdate("update person set salary = 100 where username = '苟犇'");
+            //查询操作  Query  返回值 是一个结果集
+            ResultSet rs =  stat.executeQuery("select *from peroson");
+            while(rs.next()){
+                String name = rs.getString("username");
+                String address = rs.getString("address");
+                //int s = rs.getInt("salary");
+                String dept =  rs.getString("department");
+                System.out.println(name + "-"+address+"-"+"-"+dept);
+            }
             //6.关闭 (先创建的后关闭)
+            rs.close();
             stat.close();
             conn.close();
         } catch (ClassNotFoundException e) {
@@ -34,7 +40,5 @@ public class TestJDBC {
             e.printStackTrace();
         }
 
-        HashSet set = new HashSet();
-        
     }
 }
