f59821068e74aa0312a54666c93bedde3291faad
苟犇 <815166607@qq.com>
2020-03-15 23:51:34 +0800

    提交代码

---
+++ LeetCode/src/array/ArrayTest.java
@@ -0,0 +1,43 @@
+public class ArrayTest{
+
+
+//给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + //c = 0 ？请你找出所有满足条件且不重复的三元组。
+
+//注意：答案中不可以包含重复的三元组。
+
+        public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> lists = new ArrayList<>();
+        //排序
+        Arrays.sort(nums);
+        //双指针
+        int len = nums.length;
+        for(int i = 0;i < len;++i) {
+            if(nums[i] > 0) return lists;
+
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+
+            int curr = nums[i];
+            int L = i+1, R = len-1;
+            while (L < R) {
+                int tmp = curr + nums[L] + nums[R];
+                if(tmp == 0) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(curr);
+                    list.add(nums[L]);
+                    list.add(nums[R]);
+                    lists.add(list);
+                    while(L < R && nums[L+1] == nums[L]) ++L;
+                    while (L < R && nums[R-1] == nums[R]) --R;
+                    ++L;
+                    --R;
+                } else if(tmp < 0) {
+                    ++L;
+                } else {
+                    --R;
+                }
+            }
+        }
+        return lists;
+    }
+
+}
