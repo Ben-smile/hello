31f34b899c803f58ad979b379d516121f646a7f0
苟犇 <815166607@qq.com>
2020-05-07 23:49:46 +0800

    提交代码

--- server/testbrowserandserver/server/src/server/Server.java
+++ server/testbrowserandserver/server/src/server/Server.java
@@ -9,8 +9,10 @@ public class Server {
     public void startServer(){
         try {
             System.out.println("====启动服务器====");
+            //获取配置文件中的port号码
+            int port = Integer.parseInt(ServerFileReader.getValue("port"));
             //自己创建一个服务
-            ServerSocket server = new ServerSocket(9999);
+            ServerSocket server = new ServerSocket(port);
             while(true){
                 //等待某一个客户端过来连接
                 Socket socket = server.accept();
