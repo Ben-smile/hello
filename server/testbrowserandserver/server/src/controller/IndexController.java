8c0c7dbd6b89718d8f2f6a6789793a702b750025
苟犇 <815166607@qq.com>
2020-05-08 23:44:38 +0800

    提交代码

--- server/testbrowserandserver/server/src/controller/IndexController.java
+++ server/testbrowserandserver/server/src/controller/IndexController.java
@@ -6,8 +6,7 @@ import server.HttpServletResponse;
 
 public class IndexController extends HttpServlet {
     //一个控制层
-
-    public void service(HttpServletRequest request, HttpServletResponse response){
+        public void service(HttpServletRequest request, HttpServletResponse response){
         //1.获取请求发送过来携带的参数?
         //2.找到某一个业务层的方法 做事
         //3.将最终业务层执行完毕的结果 交还给服务器 让服务器写回给浏览器
