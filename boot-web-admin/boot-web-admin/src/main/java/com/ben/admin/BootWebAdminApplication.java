8b5557d31a6e1a92ab84752c6841183ab0b8c00f
苟犇 <815166607@qq.com>
2021-03-12 17:58:09 +0800

    提交代码

---
+++ boot-web-admin/boot-web-admin/src/main/java/com/ben/admin/BootWebAdminApplication.java
@@ -0,0 +1,13 @@
+package com.ben.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootWebAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootWebAdminApplication.class, args);
+    }
+
+}
